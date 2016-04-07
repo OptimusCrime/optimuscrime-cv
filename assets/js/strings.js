344dd7f406e048a7cabd6574df14c6a30a809cb6
OptimusCrime <thomasgautv@hotmail.com>
2016-04-07 02:03:04 +0200

    Add more translations

--- assets/js/strings.js
+++ assets/js/strings.js
@@ -119,7 +119,7 @@ var strings = [
         ,en: '2011 &ndash; Present'
         ,no: '2011 &ndash; Nåværende'
     }, {
-        selector: '.translate-job-ringeriksbadet1 p'
+        selector: '.translate-job-ringeriksbadet1-description p'
         ,en: 'After working at Ringeriksbadet for one year it was decided that they needed a new web page. I had already developed their iternal, and was asked if I wanted the task. The design was created by <a href="http://www.newdesign.no" target="_blank">New Design</a>, and the rest was done by me.'
         ,no: 'Etter å ha vært ansatt på Ringeriksbadet ett år ble det bestemt at de trengte nye nettsider. Jeg hadde allerede utviklet en intranett-løsning som de brukte, og ble spurt om jeg ville ha oppdraget. Designet på nettsiden er laget av <a href="http://www.newdesign.no" target="_blank">New Design</a>, og resten er gjort av meg.'
     }
@@ -133,7 +133,7 @@ var strings = [
         ,en: 'Lifeguard'
         ,no: 'Badevert'
     }, {
-        selector: '.translate-job-ringeriksbadet1 p'
+        selector: '.translate-job-ringeriksbadet2-description p'
         ,en: 'Position includes work as a life guard, receptionist, and kitchen employee.'
         ,no: 'Arbeidstittelen badevert inkluderer arbeidsoppgaver som badevakt, resepsjonist og kjøkkenarbeid.'
     }
@@ -147,7 +147,7 @@ var strings = [
         ,en: 'Receptionist / Chief / Maid service'
         ,no: 'Resepsjonist / Kokk / Vaskehjelp'
     }, {
-        selector: '.translate-job-utvika p'
+        selector: '.translate-job-utvika-description p'
         ,en: 'My summer job \'10.'
         ,no: 'Min sommerjobb 2010.'
     }
@@ -170,6 +170,30 @@ var strings = [
         selector: '.translate-skills-heading'
         ,en: 'Skills'
         ,no: 'Ferdigheter'
+    }, {
+        selector: '.translate-skills-html-description p'
+        ,en: 'I\'ve been playing with websites since I was attending elementary school, and hold a lot of experience with both HTML and CSS.'
+        ,no: 'Jeg har lekt med hjemmesider siden jeg gikk på barneskolen og har i dag mye erfaring med både HTML og CSS.'
+    }, {
+        selector: '.translate-skills-js-description p'
+        ,en: 'In addition to HTML and CSS have I\'ve also been playing with JavaScript. This includes general knowledge about the language, as well as well-known libraries and frameworks like <a href="http://jquery.com" target="_blank">jQuery</a> and <a href="http://www.sencha.com/products/extjs/" target="_blank">Ext js</a>.'
+        ,no: 'I tillegg til HTML og CSS har jeg også lekt meg mye med JavaScript. Dette inkluderer generell kunnskap om språket, samt erfaring med kjente biblioteker og rammeverk som <a href="http://jquery.com" target="_blank">jQuery</a> og <a href="http://www.sencha.com/products/extjs/" target="_blank">Ext js</a>.'
+    }, {
+        selector: '.translate-skills-php-description p'
+        ,en: 'PHP was the first programming language I began to learn. I\'ve developed everything from small projects to Facebook applications, and plugins to well-known frameworks.'
+        ,no: 'PHP var det første programmeringsspråket jeg begynte å lære. I PHP har jeg utvklet alt fra småprosjekter til Facebook-applikasjoner og plugins for kjente rammeverk.'
+    }, {
+        selector: '.translate-skills-python-java-android-description p'
+        ,en: 'Both Python and Java are programming languages I\'ve learned at university. I\'ve also used Python a lot to spare time projects, and I have experience with frameworks such as <a href="https://www.djangoproject.com" target="_blank">Django</a> and <a href="http://flask.pocoo.org" target="_blank">Flask</a>. I also developed an Android application for Catch Media.'
+        ,no: 'Både Python og Java er språk jeg hovedsaklig har lært gjennom studiet på NTNU. Jeg har også brukt Python mye til fritidsprosjekter og har erfaring med kjente rammeverk som <a href="https://www.djangoproject.com" target="_blank">Django</a> og <a href="http://flask.pocoo.org" target="_blank">Flask</a>. For Catch Media har jeg også utviklet en Android app.'
+    }, {
+        selector: '.translate-skills-database-description p'
+        ,en: 'I have experience with database systems such as <a href="https://www.mysql.com" target="_blank">MySQL</a>, <a href="http://www.postgresql.org" target="_blank">PostgreSQL</a>, and <a href="https://products.office.com/en-us/access" target="_blank">Access</a>.'
+        ,no: 'Jeg har erfaring med databasesystemer som <a href="https://www.mysql.com" target="_blank">MySQL</a>, <a href="http://www.postgresql.org" target="_blank">PostgreSQL</a> og <a href="https://products.office.com/en-us/access" target="_blank">Access</a>.'
+    }, {
+        selector: '.translate-skills-other-description p'
+        ,en: 'In addition to the things listed above do I have some experience with Objective-C, iPhone applications, C# and a little C++.'
+        ,no: 'I tillegg til tingene listet ovenfor har jeg også litt erfaring med Objective-C og iPhone-applikasjoner, C# og litt C++.'
     }
     
     /**
@@ -180,16 +204,17 @@ var strings = [
         selector: '.translate-various-heading'
         ,en: 'Various'
         ,no: 'Diverse'
+    }, {
+        selector: '.translate-various-os-description p'
+        ,en: 'I have experience with Windows, Mac OS X, and the Linux distributions Debian and Ubuntu.'
+        ,no: 'Jeg har erfaring med både Windows, Mac OS X og Linux-distribusjonene Debian og Ubuntu.'
+    }, {
+        selector: '.translate-various-sysadmin-description p'
+        ,en: 'As a programmer have I also gained some experience in simple sysadmin related tasks. This includes setting up a webserver, settig up routine tasks like monitoring and backup solutions. I still have a lot to lear on this field, but I feel that I can Google my way to solutions on problems that are not too advanced.'
+        ,no: 'Som programmerer har jeg også gjort enkle sysadminrelaterte oppgaver. Dette inkluderer oppsett av webservere, enkelte rutinemessige oppgaver som monitoring og backupløsninger. Har fortsatt mye igjen å lære på dette feltet, men føler jeg kan Google meg frem til løsninger på ting som ikke er alt for avansert.'
+    }, {
+        selector: '.translate-various-adobe-description p'
+        ,en: 'I also have experience with typics Adobe programs such as Photoshop, Illustrator and InDesign. I don\'t hav the knowledge to produce graphics myself, but I master the programs to some degree and can export and manipulate simple graphics.'
+        ,no: 'Jeg har også erfaring med typiske Adobeprogrammer som Photoshop, Illustrator og InDesign. Kunnskaper til å lage grafikk selv har jeg ikke, men jeg behersker programmene og kan hente ut og manipulere enkel grafikk.'
     }
-
-    /*
-    , {
-        selector: '.xxxxx'
-        ,en: 'eeeeee'
-        ,no: 'eeee'
-    }*/
-    
-
-    
-    
 ];
